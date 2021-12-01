f8311ce641378f5f0f15870be4e959a57d550016
RachelSparto <rmbachman2016@hotmail.com>
2021-12-01 16:54:37 -0500

    first test in caesar

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,17 @@
 // Write your tests here!
+//import ceaser
+const caesar = require("../src/caesar.js");
+
+const expect = require("chai").expect;
+describe("Caesar", () => {
+  it("It returns false if the shift value is equal to 0, less than -25, greater than 25, or not present", () => {
+    const firstCase = caesar("string", 0);
+    const secondCase = caesar("string", -26);
+    const thirdCase = caesar("string", 26);
+    const fourthCase = caesar("string");
+    expect(firstCase).to.be.false;
+    expect(secondCase).to.be.false;
+    expect(thirdCase).to.be.false;
+    expect(fourthCase).to.be.false;
+  });
+});
