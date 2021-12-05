7852ffd52c72e863d1b5a27977ba6f62df7b3ed5
RachelSparto <rmbachman2016@hotmail.com>
2021-12-05 11:30:57 -0500

    substitution second test written: phrase correctly translated

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -2,3 +2,14 @@
 const substitution = require("../src/substitution.js").substitution;
 
 const expect = require("chai").expect;
+describe("substitution", () => {
+  it("It returns false if the given alphabet isn't exactly 26 characters long", () => {
+    const actual = substitution("abc", "bcadefghijklmnopqrstuvwxy");
+    expect(actual).to.be.false;
+  });
+  it("It correctly translates the given phrase, based on the alphabet given to the function", () => {
+    const actual = substitution("abc", "bcadefghijklmnopqrstuvwxyz");
+    const expected = "bca";
+    expect(actual).to.eql(expected);
+  });
+});
